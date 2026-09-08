5fead4034d3e3dab5358313bd7f38f456e802c70
Pratiksha Patil <pratikshapatil01312@gmail.com>
2026-09-08 23:21:01 +0530

    Day 3: Solve Binary Search problem

---
+++ Day3/README.md
@@ -0,0 +1,28 @@
+# Day 3 - Binary Search
+
+## Problem
+Find the index of a target element in a sorted array using Binary Search.
+
+## Example
+
+Array: [10, 20, 30, 40, 50, 60, 70]
+
+Target: 60
+
+Output: 5
+
+## What I learned
+
+- Binary Search
+- Sorted arrays
+- Low, high and mid
+- While loop
+- Searching efficiently
+
+## Time Complexity
+
+O(log n)
+
+## Space Complexity
+
+O(1)
---
+++ Day3/binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+target = 60
+
+result = binary_search(arr, target)
+
+print("Target index:", result)
