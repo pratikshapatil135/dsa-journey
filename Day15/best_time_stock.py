1a4c28e0bfbaca8f9662a3dae2b98aa02907f631
Pratiksha Patil <pratikshapatil01312@gmail.com>
2026-09-20 23:08:06 +0530

    Day 15: Best Time to Buy and Sell Stock

---
+++ Day15/best_time_stock.py
@@ -0,0 +1,21 @@
+def max_profit(prices):
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+
+        profit = price - min_price
+
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+
+prices = [7, 1, 5, 3, 6, 4]
+
+result = max_profit(prices)
+
+print("Maximum profit:", result)
